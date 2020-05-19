0bb3307cb2be53185752fef09009504198592c46
Suabicii <michal.slabik3@wp.pl>
2020-05-19 13:52:33 +0200

    Fixed dropdown menu

--- balance.js
+++ balance.js
@@ -1,12 +1,22 @@
-let dropdownHeadingBtn = document.querySelector('.dropdown-heading');
-let dropdownOptions = document.querySelectorAll('.dropdown-option');
+let dropdownHeadingBtn = document.querySelector(".dropdown-heading");
+let dropdownOptions = document.querySelectorAll(".dropdown-option");
 
 const toggleMenuOptions = () => {
-    dropdownOptions.forEach(option => {
-        if (option.style.display == "block") option.style.display = "none";
-        else option.style.display = "block";
-    });
-}
+  dropdownOptions.forEach((option) => {
+    if (option.style.display == "block") option.style.display = "none";
+    else option.style.display = "block";
+  });
+};
+
+const chooseMenuOption = () => {
+  dropdownOptions.forEach((option) =>
+    option.addEventListener("click", () => {
+      dropdownHeadingBtn.textContent = option.textContent;
+      toggleMenuOptions();
+    })
+  );
+};
 
 dropdownHeadingBtn.addEventListener("mouseover", toggleMenuOptions);
 dropdownHeadingBtn.addEventListener("click", toggleMenuOptions);
+chooseMenuOption();
